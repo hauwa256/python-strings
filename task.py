2f5de9e35de12d475090b94dd938600e8b2c18b7
hauwa256 <167862921+hauwa256@users.noreply.github.com>
2024-04-23 11:37:29 +0100

    Add files via upload

--- task.py
+++ task.py
@@ -1,5 +1,7 @@
 text = "pneumonoultramicroscopicsilicovolcanoconiosis"
 print(text[:7])
 print(text[:-10])
-print(text[:25])
+print(text[18:25])
+print(text[int(len(text) /2)])
+print(len(text[:])/len(text[:3]))
 
